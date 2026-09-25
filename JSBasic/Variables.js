ab3861beeb94b4d2b3e81e6a90a45918fb762ab0
Shadab Hussain <shadabhussain7864@gmail.com>
2026-09-25 13:44:15 +0530

    pushing file to new branch main

--- JSBasic/Variables.js
+++ JSBasic/Variables.js
@@ -1,2 +1,20 @@
-var a = "100";
-console.log(a); // Output: 100
+// We will be learning about variable and there types 
+/*
+    we have three different types of variables declaration
+    1 - var -- when we need to update something globally
+    2 - let -- we need to define within global Scope
+    3 - const -- we don't need to change anywhere else in program 
+*/
+// Declaration of variable is same whether we use var / let or const
+var age = 25;
+var str = "Kaiizen";
+var char = 'C';
+var salary = 10.2;
+let age = 25;
+let str = "Kaiizen";
+let char = 'C';
+let salary = 10.2;
+const age = 25;
+const str = "Kaiizen";
+const char = 'C';
+const salary = 10.2;
